7a60e33e1ea48fbce0b524284ff21060be3a0b8a
Nobuo Saeki <nobuo.saeki@gmail.com>
2015-01-16 21:55:03 +0900

    Removed script

--- util/cron.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Sample script to run collector as daily cron.
-# Set up this like this:
-# 0 4 * * * /path/to/this/repo/util/cron.sh
-
-cd `dirname $0`/..
-bundle exec bin/collect_data.rb target/*
